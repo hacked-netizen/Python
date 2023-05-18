4480758c63dadc0546ff089c82b29b1fcfef0989
Utkarsh Swarup <76107260+hacked-netizen@users.noreply.github.com>
2023-05-18 23:09:23 +0530

    Add files via upload

---
+++ mysql/query3.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+mydb=mysql.connector.connect( host="localhost",user="root",passwd="1234",database="example")
+
+mycursor=mydb.cursor()
+
+mycursor.execute("select stream from student group by stream having count(stream)>1")
+
+x= mycursor.fetchall()
+
+print(x)
+
