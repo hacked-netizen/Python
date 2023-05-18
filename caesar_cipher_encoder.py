227401d926da204b9afc14db2ff600aec959187f
Utkarsh Swarup <76107260+hacked-netizen@users.noreply.github.com>
2023-05-19 00:42:15 +0530

    Add files via upload

---
+++ caesar_cipher_encoder.py
@@ -0,0 +1,26 @@
+def caesar_cipher(text,s):
+    result = ""
+ 
+    # traverse text
+    for i in range(len(text)):
+        char = text[i]
+ 
+        #Encrypt uppercase characters
+        if (char.isupper()):
+            result += chr((ord(char) + s-65) % 26 + 65)
+ 
+        #Encrypt lowercase characters
+        else:
+            result += chr((ord(char) + s - 97) % 26 + 97)
+ 
+    return result
+ 
+#check the above function
+text = "ATTACKonTITAN"
+text1 = "EXXEGOsrXMXER"
+s = 22
+print("Text  : " + text)
+print("Shift : " + str(s))
+print("Cipher: " + caesar_cipher(text1,s))
+           
+
